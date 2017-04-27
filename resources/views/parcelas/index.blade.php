b3cf7a91675a9102694874a64b518eab0b9e12c4
Lucas Marques <lucasmarques73@hotmail.com>
2017-04-27 10:36:33 -0300

    Inserindo Modal para Efetuar o Pagamento

--- resources/views/parcelas/index.blade.php
+++ resources/views/parcelas/index.blade.php
@@ -19,6 +19,7 @@
                       <th>Data de Pagamento</th>
                       <th>Valor da Parcela</th>
                       <th>Valor Pago</th>
+                      <th>Opções</th>
                       <th></th>
                     </thead>
                     <tbody>
@@ -31,6 +32,7 @@
                         <td>{{$parcela->data_pagamento}}</td>
                         <td>{{$parcela->valor_parcela}}</td>
                         <td>{{$parcela->valor_pago}}</td>
+                        <td><button class="btn btn-success" data-toggle="modal" data-target="#myModal">Pagar</button></td>
                       </tr>
                     @endforeach
                     </tbody>
@@ -41,3 +43,43 @@
     </div>
 </div>
 @endsection
+
+<!-- Modal -->
+ <div class="modal fade" id="myModal" role="dialog">
+   <div class="modal-dialog">
+     {!! Form::open(['route' => 'parcelas.salvar', 'method' => 'POST']) !!}
+       <!-- Modal content-->
+       <div class="modal-content">
+         <div class="modal-header">
+           <button type="button" class="close" data-dismiss="modal">&times;</button>
+           <h4 class="modal-title">Dados do Pagamento</h4>
+         </div>
+
+           <div class="modal-body">
+             <div class="container-fluid">
+               <div class="col-md-6">
+                 {!! Form::label('data_vencimento', 'Data de Vencimento') !!}
+                 {!! Form::input('date', 'data_vencimento', null, ['class' => 'form-control'])!!}
+
+                 {!! Form::label('valor_parcela', 'Valor da Parcela') !!}
+                 {!! Form::input('number', 'valor_parcela', null, ['class' => 'form-control'])!!}
+
+               </div>
+               <div class="col-md-6">
+                 {!! Form::label('data_pagamento', 'Data de Pagamento') !!}
+                 {!! Form::input('date', 'data_pagamento', null, ['class' => 'form-control'])!!}
+
+                 {!! Form::label('valor_pago', 'Valor Pago') !!}
+                 {!! Form::input('number', 'valor_pago', null, ['class' => 'form-control'])!!}
+
+               </div>
+             </div>
+           </div>
+           <div class="modal-footer">
+             {!! Form::submit('Pagar', ['class' => 'btn btn-success', 'data-dismiss' => 'modal'])!!}
+           </div>
+
+       </div>
+     {!! Form::close() !!}
+   </div>
+ </div>
