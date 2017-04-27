43191231c352b1d245001e587e75d551a5fa4607
Lucas Marques <lucasmarques73@hotmail.com>
2017-04-27 13:39:52 -0300

    Adicionando imagem ao pdf

--- resources/views/parcelas/pdf.blade.php
+++ resources/views/parcelas/pdf.blade.php
@@ -4,7 +4,12 @@
     <meta charset="utf-8">
     <title>Parcelas</title>
     <style>
-    table {
+    img{
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+    }
+    table{
       width: 100%;
       border-collapse: collapse;
     }
@@ -14,43 +19,31 @@
     </style>
   </head>
   <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                      Parcelas
-                    </div>
-
-                    <table class="table">
-                        <thead>
-                          <th>Movimentação</th>
-                          <th>Status</th>
-                          <th>Número da Parcela</th>
-                          <th>Data de Vencimento</th>
-                          <th>Data de Pagamento</th>
-                          <th>Valor da Parcela</th>
-                          <th>Valor Pago</th>
-                          <th></th>
-                        </thead>
-                        <tbody>
-                          @foreach($parcelas as $parcela)
-                          <tr>
-                            <td>{{$parcela->movimento_id}}: {{ $parcela->Movimento->descricao }}</td>
-                            <td>{{$parcela->status}}</td>
-                            <td>{{$parcela->numero_parcela}}</td>
-                            <td>{{$parcela->data_vencimento}}</td>
-                            <td>{{$parcela->data_pagamento}}</td>
-                            <td>{{$parcela->valor_parcela}}</td>
-                            <td>{{$parcela->valor_pago}}</td>
-                          </tr>
-                        @endforeach
-                        </tbody>
-                      </table>
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <img src="assets/images/logo.png" alt="">
+      <table>
+          <thead>
+            <th>Movimentação</th>
+            <th>Status</th>
+            <th>Número da Parcela</th>
+            <th>Data de Vencimento</th>
+            <th>Data de Pagamento</th>
+            <th>Valor da Parcela</th>
+            <th>Valor Pago</th>
+            <th></th>
+          </thead>
+          <tbody>
+            @foreach($parcelas as $parcela)
+            <tr>
+              <td>{{$parcela->movimento_id}}: {{ $parcela->Movimento->descricao }}</td>
+              <td>{{$parcela->status}}</td>
+              <td>{{$parcela->numero_parcela}}</td>
+              <td>{{$parcela->data_vencimento}}</td>
+              <td>{{$parcela->data_pagamento}}</td>
+              <td>{{$parcela->valor_parcela}}</td>
+              <td>{{$parcela->valor_pago}}</td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
   </body>
 </html>
