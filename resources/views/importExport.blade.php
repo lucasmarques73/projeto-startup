9cbd76bab348bc394138a47b3ec3d1e269fe1cab
Lucas Marques <lucasmarques73@hotmail.com>
2017-05-17 18:28:40 -0300

    View para importar os relatórios

--- resources/views/importExport.blade.php
+++ resources/views/importExport.blade.php
@@ -12,13 +12,44 @@
 		</div>
 	</nav>
 	<div class="container">
-		<a href="{{ URL::to('downloadExcel/xls') }}"><button class="btn btn-success">Download Excel xls</button></a>
-		<a href="{{ URL::to('downloadExcel/xlsx') }}"><button class="btn btn-success">Download Excel xlsx</button></a>
-		<a href="{{ URL::to('downloadExcel/csv') }}"><button class="btn btn-success">Download CSV</button></a>
-		<form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="{{ URL::to('importExcel') }}" class="form-horizontal" method="post" enctype="multipart/form-data">
-			<input type="file" name="import_file" />
-			<button class="btn btn-primary">Import File</button>
-		</form>
+		<div class="row">
+			<div class="col-3">
+				<h1>Importar Associados</h1>
+
+				{!! Form::open(['url' => 'importAss', 'method' => 'POST','files' => true ]) !!}
+
+		        {!! Form::file('import_file_ass', null) !!}
+
+				{!! Form::submit('Upload', ['class' => 'btn btn-primary']) !!}
+
+				{!! Form::close() !!}
+			</div>
+
+			<div class="col-3">
+				<h1>Importar End Associados</h1>
+
+				{!! Form::open(['url' => 'importAssEnd', 'method' => 'POST','files' => true ]) !!}
+
+		        {!! Form::file('import_file_ass_end', null) !!}
+
+				{!! Form::submit('Upload', ['class' => 'btn btn-primary']) !!}
+
+				{!! Form::close() !!}
+			</div>
+
+			<div class="col-3">
+				<h1>Importar Veículos</h1>
+
+				{!! Form::open(['url' => 'importVec', 'method' => 'POST','files' => true ]) !!}
+
+		        {!! Form::file('import_file_vec', null) !!}
+
+				{!! Form::submit('Upload', ['class' => 'btn btn-primary']) !!}
+
+				{!! Form::close() !!}
+			</div>
+
+		</div>
 	</div>
 </body>
 </html>
